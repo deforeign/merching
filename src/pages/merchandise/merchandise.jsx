e41e1eb6f682bdcf3f4b8c05eac6c44c53de805f
sparshjaggi07 <sparshjaggi2607@gmail.com>
2024-12-15 15:40:37 +0530

    Merch Updated

--- src/pages/merchandise/merchandise.jsx
+++ src/pages/merchandise/merchandise.jsx
@@ -68,7 +68,7 @@ function Merchandise() {
 	  const imgarray ={
 		1: "./src/assets/customIllustration/oc_remove.png",
 		2: "./src/assets/customIllustration/hc_remove.png",
-		3: "./src/assets/customIllustration/cc_remove.png",
+		3: "./src/assets/customIllustration/cc._remove.png",
 		4: "./src/assets/customIllustration/exe_remove.png"
 	  }
 	  
