ee4e2b813d6806272ddc581ca23349ca93f1953e
Aman Kumar <trulyaman25@gmail.com>
2024-12-16 12:33:29 +0530

    Deployment Issue Fixed Part - 1

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import HomeBackground from '../../components/homeBackground';
 import MatrixBackground from '../../components/matrixBackground';
 import LandingSection from './homeComponents/landingSection';
-import ImageSection from './homeComponents/ImageSection';
+import ImageSection from './homeComponents/imageSection';
 
 function Home() {
     return (
